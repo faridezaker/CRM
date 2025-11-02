47c28680158482a8a5bbc63b752243a29cfabbb3
unknown <faridezaker9730@gmail.com>
2025-11-02 11:02:37 +0330

    add swagger package and add swagger to register,login endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="CRM API Documentation",
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
